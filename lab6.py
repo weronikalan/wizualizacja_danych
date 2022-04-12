533442274df8c095d128cd883d26297d5752c3ed
weronikalan <100271774+weronikalan@users.noreply.github.com>
2022-04-12 18:56:11 +0200

    Add files via upload

---
+++ lab6.py
@@ -0,0 +1,62 @@
+import numpy as np
+import math
+#zad1
+# a = np.arange(4,160,4)
+# print(a)
+
+#zad2
+# lista = np.array([np.arange(2), np.arange(2)], dtype = 'float')
+# lista2 = lista.astype('int32')
+# print(lista)
+# print(lista2)
+
+#zad3
+# def funkcja(n):
+#     A = np.arange(n*n).reshape((n, n))
+#     return A
+#
+# print(funkcja(3))
+
+
+#zad4
+# def generuj(a, b):
+#     a for a in range(b):
+#         funkcja = np.logspace(a**b)
+#     return funkcja
+#
+# print(generuj(2, 4))
+
+#zad5
+# def funkcja(a):
+#     b = np.arange(a)
+#     c = np.diag([x for x in range(a)], 2)
+#     return c
+#
+# print(funkcja(6))
+
+#zad6
+# a = np.array([['k',1,1,1,1,1,1,1], ['o', 1,'d',1, 's', 'e', 'i', 'p'], ['t',1,1,'o',1,1,1,1], [1,1,1,1,'m',1,1,1]])
+# print(a)
+
+#zad7
+# def funkcja(n):
+#     a = np.array([[2,4,6],[4,2,4],[6,4,2]])
+#     return a
+#
+# print(funkcja(3))
+
+#zad8
+# def funkcja(a = np.arange(4), kierunek):
+#     if kierunek == 'x':
+#         n = slice(2,2,2)
+#     elif kierunek == 'y':
+#         m = slice(2,2,2)
+#     else
+#
+# print(a[n])
+# print(a[m])
+
+#zad9
+# a = np.arange(25)
+# a = a.reshape((5, 5)) * 2 + 1
+# print(a)
